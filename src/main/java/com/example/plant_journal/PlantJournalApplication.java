33f8b3a76c6355fe7e908c4d47d890cf61c6af6d
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2025-02-02 23:28:07 +0900

    環境設定

---
+++ src/main/java/com/example/plant_journal/PlantJournalApplication.java
@@ -0,0 +1,13 @@
+package com.example.plant_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plant_journal/PlantJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plant_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
